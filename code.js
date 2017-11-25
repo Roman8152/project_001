94f7286bd16dc6ec5ef2f4b55e5ee7bbeab3b7ec
Roman8152 <romanic654@email.cz>
2017-11-25 20:41:30 +0100

    asd

--- code.js
+++ code.js
@@ -2,26 +2,34 @@ var Fighter1 = {
     atk: 30,
     def: 5,
     hp: 100,
-    move: 1,
+    healMove: 0,
     attack: function() {
         var attack1 = (this.atk + (Math.floor(Math.random() * 5) - 5) - Fighter2.def);
         Fighter2.hp = Fighter2.hp - attack1;
         document.getElementById("hp2").innerHTML = Fighter2.hp;
-        --this.move;
-        ++Fighter2.move;
+        ++this.healMove;
+        document.getElementById("plr1heal").disabled = true;
+        document.getElementById("plr1atk").disabled = true;
+        document.getElementById("plr2atk").disabled = false;
         if (Fighter2.hp <= 0) {
             document.getElementById("hp1").innerHTML = "WINNER";
             document.getElementById("hp2").innerHTML = "DEAD";
         }
-        if (this.move === 0) {
-            document.getElementById("plr1atk").disabled = true;
-            document.getElementById("plr1heal").disabled = true;
-        }    
+        if (Fighter2.healMove >= 3) {
+            document.getElementById("plr2heal").disabled = false;
+        }
     },
     heal: function() {
-        var heal = 5 + (Math.floor(Math.random() * 20));
-        this.hp = this.hp + heal;
-        document.getElementById("hp1").innerHTML = this.hp;
+            var heal = 5 + (Math.floor(Math.random() * 20));
+            this.hp = this.hp + heal;
+            document.getElementById("hp1").innerHTML = this.hp;
+            this.healMove = this.healMove - 3;
+            document.getElementById("plr1heal").disabled = true;
+            document.getElementById("plr1atk").disabled = true;
+            document.getElementById("plr2atk").disabled = false;
+            if (Fighter2.healMove >= 3) {
+                document.getElementById("plr2heal").disabled = false;
+            }
     }
 };
 
@@ -29,26 +37,34 @@ var Fighter2 = {
     atk: 15,
     def: 20,
     hp: 100,
-    move: 0,
+    healMove: 0,
     attack: function() {
         var attack1 = (this.atk + (Math.floor(Math.random() * 5) - 5) - Fighter1.def);
         Fighter1.hp = Fighter1.hp - attack1;
         document.getElementById("hp1").innerHTML = Fighter1.hp;
-        --this.move;
-        ++Fighter1.move;
+        ++this.healMove;
+        document.getElementById("plr2heal").disabled = true;
+        document.getElementById("plr2atk").disabled = true;
+        document.getElementById("plr1atk").disabled = false;
         if (Fighter1.hp <= 0) {
             document.getElementById("hp2").innerHTML = "WINNER";
             document.getElementById("hp1").innerHTML = "DEAD";
         }
-        if (this.move === 0) {
-            document.getElementById("plr2atk").disabled = true;
-            document.getElementById("plr2heal").disabled = true;
+        if (Fighter1.healMove >= 3) {
+            document.getElementById("plr1heal").disabled = false;
         } 
     },
     heal: function() {
         var heal = 5 + (Math.floor(Math.random() * 20));
         this.hp = this.hp + heal;
         document.getElementById("hp2").innerHTML = this.hp;
+        this.healMove = this.healMove - 3;
+        document.getElementById("plr2heal").disabled = true;
+        document.getElementById("plr2atk").disabled = true;
+        document.getElementById("plr1atk").disabled = false;
+        if (Fighter1.healMove >= 3) {
+            document.getElementById("plr1heal").disabled = false;
+        }
     }
 };
 
